7e68ea2996ff3f6372c43d7e7de5a82532c0b7c2
debesh99 <debeshpadhy99@gmail.com>
2025-04-17 15:53:36 +0530

    spring boot first commit

--- src/main/java/com/debesh/spring/budgetmanagementsystem/repository/UserRepository.java
+++ src/main/java/com/debesh/spring/budgetmanagementsystem/repository/UserRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.debesh.spring.budgetmanagementsystem.entity.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	@Query("SELECT u FROM User u WHERE u.email = ?1 AND u.password = ?2")
 	Optional<User> userLogin(String email, String password);
